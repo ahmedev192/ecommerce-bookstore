6de86f76258238d0aa6a898e2fc2f21f28f6a084
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-07-05 17:46:27 +0300

    Category edit & delete

--- ecommerce-bookstore/Controllers/CategoryController.cs
+++ ecommerce-bookstore/Controllers/CategoryController.cs
@@ -16,29 +16,108 @@ namespace ecommerce_bookstore.Controllers
         public async Task<IActionResult> Index()
         {
             List<Category> objCategoryList = await _db.Categories.ToListAsync();
-            return View(objCategoryList); 
+            return View(objCategoryList);
         }
         [HttpGet]
-        public  IActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
         public async Task<IActionResult> Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("Custom " , "Name can't be equal Display Order");
+                ModelState.AddModelError("Custom ", "Name can't be equal Display Order");
             }
 
-            if (ModelState.IsValid) { 
-            await _db.Categories.AddAsync(obj);
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Index");
-            }else
+            if (ModelState.IsValid)
+            {
+                await _db.Categories.AddAsync(obj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
             {
                 return View(obj);
             }
         }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category obj)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+
+
+            Category? obj = _db.Categories.Find(id);
+
+
+            _db.Categories.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
