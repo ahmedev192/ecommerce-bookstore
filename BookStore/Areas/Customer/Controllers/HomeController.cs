c589f1a40f2b5337839cb3e72aa1c155fe038606
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-07-09 14:37:02 +0300

    product details

--- BookStore/Areas/Customer/Controllers/HomeController.cs
+++ BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BookStore.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger , IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
@@ -25,6 +25,12 @@ namespace BookStore.Areas.Customer.Controllers
             return View(products);
 
         }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
+
 
         public IActionResult Privacy()
         {
